ccd57e6fb874e5b0927f722469652496e4313dc2
Angad Sarpotdar <sixteefour@gmail.com>
2020-10-18 12:39:20 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Space Invaders
+#Set up the screen
+#Python 3.7 windows
+import turtle
+import os
+
+#Set up the Screen
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.title("Space Invaders")
+
+delay = raw_input("Press enter to finsh")
